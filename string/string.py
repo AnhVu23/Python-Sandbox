b0ab4fcef18d2be31df401b32f198dbf9198d3a1
anhvu-95 <vu.haianh291@gmail.com>
2020-10-24 22:23:54 +0300

    Learning variable

---
+++ string/string.py
@@ -0,0 +1,3 @@
+phrase = 'Giraffe Academy'
+print(f'{phrase.lower()} is cool')
+print(f'{phrase.upper()} is more cool')
---
+++ variable/varaible.py
@@ -0,0 +1,6 @@
+character_name = 'Hai'
+character_age = 20
+print(f'There once was a man named {character_name}.')
+print(f'He was {character_age} in 1990')
+character_name = 'Mike'
+print(f'Now his name is {character_name}')
