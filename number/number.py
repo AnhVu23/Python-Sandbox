33318fbc8795f4d64ef5edd18c22a83c5c317458
anhvu-95 <vu.haianh291@gmail.com>
2020-10-24 23:05:24 +0300

    Math playground

---
+++ number/number.py
@@ -0,0 +1,19 @@
+"""
+Dictionary
+* Number playground
+📚 Resources:
+https://www.youtube.com/watch?v=rfscVS0vtbw&t=1s&ab_channel=freeCodeCamp.org
+"""
+from math import *
+
+# Calculation
+my_num = 3 * 4 + 5
+print(f'{str(my_num)}')
+# Math
+neg_num = -5
+print(abs(neg_num))
+print(pow(neg_num, 3))
+print(round(3.2))
+print(floor(3.5))
+print(ceil(3.5))
+print(sqrt(9))
