9184f262cf44582e7fb51260cc5ab034f61bf303
anhvu-95 <vu.haianh291@gmail.com>
2020-10-24 22:55:57 +0300

    Learning string

--- string/string.py
+++ string/string.py
@@ -1,3 +1,20 @@
+"""
+Dictionary
+* String playground
+📚 Resources:
+https://www.youtube.com/watch?v=rfscVS0vtbw&t=1s&ab_channel=freeCodeCamp.org
+"""
 phrase = 'Giraffe Academy'
+# Lower case
 print(f'{phrase.lower()} is cool')
+# Uppercase and check if the string is uppercase
 print(f'{phrase.upper()} is more cool')
+print(f'{phrase.upper().isupper()}')
+# Find the length of string
+print(f'Length of text: {len(phrase)}')
+# Index of a substring
+charIndex = phrase.index('ffe')
+print(f'{charIndex}')
+# String replace
+replace = phrase.replace('Giraffe', 'Cat')
+print(f'{replace}')
