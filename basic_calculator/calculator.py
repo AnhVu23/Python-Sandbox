d8c8ccb91bac460a77345bbdf178609e390b501f
Anh Vu <anh.vu@vertics.co>
2020-10-24 23:16:06 +0300

    Basic calculator playground

---
+++ basic_calculator/calculator.py
@@ -0,0 +1,14 @@
+"""
+Dictionary
+* Basic calculator application
+* Transform string to float
+📚 Resources:
+https://www.youtube.com/watch?v=rfscVS0vtbw&t=1s&ab_channel=freeCodeCamp.org
+"""
+
+num1 = input('Enter a number: ')
+num2 = input('Enter another number: ')
+
+# Parsing and calculating result
+result = float(num1) + float(num2)
+print(result)
