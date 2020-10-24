6b2b82f0f754654bbe390dcdf5839a4ed9cfc8b5
anhvu-95 <vu.haianh291@gmail.com>
2020-10-24 23:08:41 +0300

    Input playground

---
+++ input/input.py
@@ -0,0 +1,8 @@
+"""
+Dictionary
+* User Input playground
+📚 Resources:
+https://www.youtube.com/watch?v=rfscVS0vtbw&t=1s&ab_channel=freeCodeCamp.org
+"""
+name = input('Enter your name: ')
+print(f'Hello {name}')
