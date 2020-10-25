9482e369d58d5c1fe4741178c91f48ffcb469c62
Anh Vu <vu.haianh291@gmail.com>
2020-10-25 11:12:38 +0200

    Feat: Add tuple playground

--- tuples/tuples.py
+++ tuples/tuples.py
@@ -5,4 +5,9 @@ Dictionary
 https://www.youtube.com/watch?v=rfscVS0vtbw&t=1s&ab_channel=freeCodeCamp.org
 """
 
-coordinates
+# Tuples
+coordinates = (4, 5)
+
+# Throw an error when try to assign a new value to tuple => Immutable.
+coordinates[1] = 10
+print(coordinates[1])
