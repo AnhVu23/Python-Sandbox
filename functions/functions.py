39e78b3b327fb8c752b6f4b434f6bcdd1a154be9
Anh Vu <vu.haianh291@gmail.com>
2020-10-25 11:25:58 +0200

    Function playground

---
+++ functions/functions.py
@@ -0,0 +1,15 @@
+"""
+Dictionary
+* Functions playground
+📚 Resources:
+https://www.youtube.com/watch?v=rfscVS0vtbw&t=1s&ab_channel=freeCodeCamp.org
+"""
+
+
+# Define a function
+def say_hi(name, age):
+    print(f'Hello user {name}, you are {str(age)}')
+
+
+# Execute function
+say_hi('Tom', 30)
