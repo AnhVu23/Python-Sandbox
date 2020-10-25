79d5404e834a05948c566907358eadf95a5ced74
Anh Vu <vu.haianh291@gmail.com>
2020-10-25 11:49:42 +0200

    Add more advanced calculator

--- basic_calculator/calculator.py
+++ basic_calculator/calculator.py
@@ -6,9 +6,25 @@ Dictionary
 https://www.youtube.com/watch?v=rfscVS0vtbw&t=1s&ab_channel=freeCodeCamp.org
 """
 
-num1 = input('Enter a number: ')
-num2 = input('Enter another number: ')
+num1 = float(input('Enter a number: '))
+operator = input('Enter an operator: ')
+num2 = float(input('Enter another number: '))
+
+
+# Calculating result
+def calculate(first_num, second_num, op):
+    if op == '*':
+        return first_num * second_num
+    elif op == '-':
+        return first_num - second_num
+    elif op == '+':
+        return first_num + second_num
+    elif op == '/':
+        return first_num / second_num
+    else:
+        return 'Invalid operator'
+
 
 # Parsing and calculating result
-result = float(num1) + float(num2)
+result = calculate(num1, operator, num2)
 print(result)
