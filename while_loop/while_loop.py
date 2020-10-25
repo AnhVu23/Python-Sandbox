83f08fbfc4cae5081361b1f83eccdf72b2942456
Anh Vu <vu.haianh291@gmail.com>
2020-10-25 11:57:41 +0200

    While loop playground

---
+++ while_loop/while_loop.py
@@ -0,0 +1,10 @@
+"""
+Dictionary
+* While loop playground
+📚 Resources:
+https://www.youtube.com/watch?v=rfscVS0vtbw&t=1s&ab_channel=freeCodeCamp.org
+"""
+i = 1
+while i < 10:
+    print(i)
+    i += 1
