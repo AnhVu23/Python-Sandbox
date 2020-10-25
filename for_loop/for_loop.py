48926f7019857ec7fc8c572674b90a1fa983297d
Anh Vu <vu.haianh291@gmail.com>
2020-10-25 12:19:39 +0200

    For loop playground

---
+++ for_loop/for_loop.py
@@ -0,0 +1,14 @@
+"""
+Dictionary
+* For loop playground
+📚 Resources:
+https://www.youtube.com/watch?v=rfscVS0vtbw&t=1s&ab_channel=freeCodeCamp.org
+"""
+
+friends = ['Jin', 'Karen', 'Kevin']
+for friend in friends:
+    print(friend)
+
+# Looping through array using index.
+for index in range(len(friends)):
+    print(friends[index])
