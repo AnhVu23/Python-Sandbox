684620d99796f34c49473e9639dc4a5a91d1b6fb
Anh Vu <vu.haianh291@gmail.com>
2020-10-25 13:01:57 +0200

    Check uppercase for input

--- translator/translator.py
+++ translator/translator.py
@@ -13,7 +13,10 @@ def translate(phrase):
     translation = ''
     for letter in phrase:
         if letter.lower() in vowel:
-            translation = translation + 'g'
+            if letter.isupper():
+                translation = translation + 'G'
+            else:
+                translation = translation + 'g'
         else:
             translation = translation + letter
     return translation
