2893bd16bda927451345961fcf77b06d7d95de34
Anh Vu <vu.haianh291@gmail.com>
2020-10-26 22:27:23 +0200

    Write and append file

--- files/file.py
+++ files/file.py
@@ -7,7 +7,7 @@ https://www.youtube.com/watch?v=rfscVS0vtbw&t=1s&ab_channel=freeCodeCamp.org
 """
 
 # Read file
-test_file = open('test.txt', 'r')
+test_file = open('./files/test.txt', 'r')
 
 print(test_file.readable())
 # Read a single line
@@ -15,3 +15,16 @@ print(test_file.readline())
 # Read all the lines
 print(test_file.readlines())
 test_file.close()
+
+
+# Write a new file
+new_test_file = open('./files/test.txt', 'w')
+new_test_file.write('Some more test')
+new_test_file.write('\nNext line')
+new_test_file.close()
+
+# Append to a file
+append_test_file = open('./files/test.txt', 'a')
+append_test_file.write('Some more test')
+append_test_file.write('\nNext line')
+append_test_file.close()
