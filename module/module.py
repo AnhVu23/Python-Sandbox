498bf8f55b761b42e015f453ab66eb1e57a0e885
Anh Vu <vu.haianh291@gmail.com>
2020-10-26 22:43:21 +0200

    Module and pip playground

---
+++ module/module.py
@@ -0,0 +1,9 @@
+"""
+Dictionary
+* Module playground
+📚 Resources:
+https://www.youtube.com/watch?v=rfscVS0vtbw&t=1s&ab_channel=freeCodeCamp.org
+"""
+import useful_tools
+
+print(useful_tools.roll_dice(10))
